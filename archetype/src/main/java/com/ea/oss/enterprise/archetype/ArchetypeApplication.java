416a0af543a16bb07e7076bb30899f7669a11da3
amit169 <amit.lalloo@standardbank.co.za>
2018-07-24 21:24:34 +0200

    modified ArchetypeApplication.java to include Eureka Client

--- archetype/src/main/java/com/ea/oss/enterprise/archetype/ArchetypeApplication.java
+++ archetype/src/main/java/com/ea/oss/enterprise/archetype/ArchetypeApplication.java
@@ -1,12 +1,30 @@
 package com.ea.oss.enterprise.archetype;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@Configuration
+@EnableAutoConfiguration
+@EnableEurekaClient
+@RestController
 @SpringBootApplication
 public class ArchetypeApplication {
+
+	@RequestMapping("/")
+	public String home(){
+		return "Hello World!";
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(ArchetypeApplication.class, args);
 	}
+
+
+
 }
