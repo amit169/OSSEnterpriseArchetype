2dd7908234260924ee4af63d66fc6ce66f9e30b6
amit169 <amit.lalloo@standardbank.co.za>
2018-07-24 15:28:32 +0200

    added master project files :q :#

---
+++ archetype/src/main/java/com/ea/oss/enterprise/archetype/ArchetypeApplication.java
@@ -0,0 +1,12 @@
+package com.ea.oss.enterprise.archetype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchetypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchetypeApplication.class, args);
+	}
+}
---
+++ archetype/src/test/java/com/ea/oss/enterprise/archetype/ArchetypeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ea.oss.enterprise.archetype;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ArchetypeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
