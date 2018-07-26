7e18b8b3b5d83285956a858f463384e4570845e3
amit169 <amit.lalloo@standardbank.co.za>
2018-07-26 11:22:42 +0200

    added testCLient for circuitBreaker

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # OSSEnterpriseArchetype
 Provides Boiler Plate Code for Developing Enterprise Applications leveraging Open Source  
+
+
+Step 1 : Pull the repository
+
+Step 2: Start a eureka Sever {
+  Docker pull 
+}
+
+Step 3 : Run Spring Boot Application
---
+++ clientCircuitBreaker /src/main/java/com/ea/callingClient/clientCircuitBreaker/ClientCircuitBreakerApplication.java
@@ -0,0 +1,12 @@
+package com.ea.callingClient.clientCircuitBreaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientCircuitBreakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientCircuitBreakerApplication.class, args);
+	}
+}
